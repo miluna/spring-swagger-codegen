d15edc0a8f09b3cd2f0fb30e02f9e0441cd40453
Miguel Angel Luna <miluna1994@gmail.com>
2018-09-11 20:19:17 +0200

    first test

---
+++ src/main/java/com/cice/CiceCodeGen/CiceCodeGenApplication.java
@@ -0,0 +1,12 @@
+package com.cice.CiceCodeGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiceCodeGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiceCodeGenApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cice/CiceCodeGen/CiceCodeGenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cice.CiceCodeGen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CiceCodeGenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
